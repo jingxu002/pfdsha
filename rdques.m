e9cab47fe87ca4a1c03f2912f97cd8f2cf6487cc
jingxu002 <jingxu@chinansc.cn>
2016-08-31 15:56:18 +0800

    code the function for reading input files

---
+++ rdques.m
@@ -0,0 +1,22 @@
+function [gfd,gpfd]=rdques(inputfile)
+% RDQUES: READ QUESTION TO PFDSHA
+% Writer: JingXu   Date:2016-08-31
+% Define Varaables:
+%{
+	inputfile		input file name, string
+	gfd			given fault displacement
+	gpfd			given annual exceedance probability of fault displacement
+%}
+
+fid=fopen(inputfile,'rt');
+
+if isequal(fid,-1)
+	error('Input file is not exist ! ');
+end
+
+ngfd=fscanf(fid,'%g',1);
+gfd=fscanf(fid,'%g',[ngfd,1]);
+ngpfd=fscanf(fid,'%g',1);
+gpfd=fscanf(fid,'%g',[ngpfd,1]);
+
+fclose(fid);
